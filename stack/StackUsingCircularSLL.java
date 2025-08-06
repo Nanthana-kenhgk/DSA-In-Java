d25aefa5d7dddc951ea7b9c2318fe4d7c2050ff2
Nanthana-kenhgk <nandinankers2@gmail.com>
2025-08-06 21:46:47 +0530

    Update StackUsingCircularSLL.java

--- stack/StackUsingCircularSLL.java
+++ stack/StackUsingCircularSLL.java
@@ -1,3 +1,8 @@
+/*
+- Stack using circular singly linked list
+- This is the code for a stack using circular singly linked list with a head and tail pointer
+- Operations: push, pop, top, isEmpty, size
+*/
 package linkedLists;
 class Node{
 	int data;
@@ -10,6 +15,7 @@ class Node{
 public class stackCircularSLL {
 	Node head, tail;
 	int length;
+	// Time complexity: O(1)
 	public void push(int data) {
 		Node new_node = new Node(data);
 		if(length==0) {
@@ -24,6 +30,7 @@ public class stackCircularSLL {
 		}
 		length++;
 	}
+	//Time complexity: O(1)
 	public int pop() {
 		if(length==0) {
 			throw new RuntimeException("Stack Underflow");
@@ -40,15 +47,18 @@ public class stackCircularSLL {
 		length--;
 		return deleted;
 	}
+	//Time complexity: O(1)
 	public int top() {
 		if(length==0) {
 			throw new RuntimeException("Stack Underflow");
 		}
 		return this.head.data;
 	}
+	//Time complexity: O(1)
 	public boolean isEmpty() {
 		return length==0;
 	}
+	//Time complexity: O(1)
 	public int size() {
 		return length;
 	}
